a05a39df7ca33669f713e9bd7b5b072449f7c06b
Connor Linfoot <connorlinfoot@me.com>
2014-08-27 10:57:47 +0100

    Fix for error when opening other chests

--- src/com/connorlinfoot/luckychest/Listeners/ChestOpen.java
+++ src/com/connorlinfoot/luckychest/Listeners/ChestOpen.java
@@ -1,5 +1,6 @@
 package com.connorlinfoot.luckychest.Listeners;
 
+import org.bukkit.Material;
 import org.bukkit.block.Chest;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,6 +15,7 @@ public class ChestOpen implements Listener {
     public void onInventory( InventoryOpenEvent e ) throws IOException {
         if( e.getInventory().getType() == InventoryType.CHEST ){
             Chest chest = (Chest) e.getInventory().getHolder();
+            if( e.getInventory().getItem(0) == null  || e.getInventory().getItem(0).getType() == Material.AIR ) return;
             if( e.getInventory().getItem(0).getItemMeta().getDisplayName().equalsIgnoreCase("LuckyChest") ){
                 e.getInventory().clear();
                 com.connorlinfoot.luckychest.Handlers.Chest.get().populateChest(chest);
